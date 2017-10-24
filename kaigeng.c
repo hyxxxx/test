dfcc6eafa3bc81e4192cbfa8a94c8ffec5794a82
huang yuxuan <2660137084@qq.com>
2017-10-24 19:43:12 +0800

    biao mian jing que

---
+++ kaigeng.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h> 
+int main(int argc, char *argv[])
+{
+	float a,b,c,d;
+	scanf("%f",&a);
+	b=a;
+	while(b<=a)
+	{
+		c=sqrt(b);
+		a=a-1;
+		d=a+c;
+		while(0<a)
+		{
+		d=sqrt(d);
+		a=a-1;
+		d=a+d;
+		}
+	}
+    printf("%.0f %20.16f\n",b++,d);
+	return 0;
+}
